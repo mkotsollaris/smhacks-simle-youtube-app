ff38c25d003e5b649450ff7e805fbcc736cd97b5
Menelaos Kotsollaris <menelaos@mkotsollaris.com>
2018-11-17 08:52:09 -0800

    update browser css

--- src/components/Browser.jsx
+++ src/components/Browser.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import VideoRenderer from "./VideoRenderer";
-import YTSearch from 'youtube-api-search'
-import {API_KEY} from './_PRIVATE_CONFIG'
+import YTSearch from "youtube-api-search";
+import { API_KEY } from "./_PRIVATE_CONFIG";
 
 class Browser extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -17,31 +16,29 @@ class Browser extends React.Component {
     this.setState({ term: event.target.value });
   }
 
-  onVideoSelectClick = (video) => {
+  onVideoSelectClick = video => {};
 
-  }
-
-  onSearchHandler = (searchTerm) => {
-    console.log('searchTerm: ',searchTerm);
-    var url = 'https://www.googleapis.com/youtube/v3/search';
+  onSearchHandler = searchTerm => {
+    console.log("searchTerm: ", searchTerm);
+    var url = "https://www.googleapis.com/youtube/v3/search";
     var params = {
-        part: 'snippet',
-        key: API_KEY,
-        q: searchTerm
+      part: "snippet",
+      key: API_KEY,
+      q: searchTerm
     };
-    console.log('this:',this);
-    YTSearch({key: API_KEY, term: searchTerm}, videos => {
+    console.log("this:", this);
+    YTSearch({ key: API_KEY, term: searchTerm }, videos => {
       this.setState({
         videos: videos
-      })
+      });
     });
-}
+  };
 
   render() {
     return (
       <div>
-        <SearchBar onChange={this.onSearchHandler}/>
-        <VideoRenderer videos={this.state.videos}/>
+        <SearchBar onChange={this.onSearchHandler} />
+        <VideoRenderer videos={this.state.videos} />
       </div>
     );
   }
